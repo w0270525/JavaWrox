8d01ae038e59a3bfbc32bdbb9bf785b19931cc1d
Jonathan Prince <prince.jon@gmail.com>
2016-01-21 23:13:06 -0400

    work on ch3, ch2 was a rehash of the program in ch1.

---
+++ Ch3/src/ch3/JamesBondCar.java
@@ -0,0 +1,23 @@
+package ch3;
+
+public class JamesBondCar extends Car {
+	int currentSubmergeDepth;
+	boolean isGunOnBoard = true;
+	final String MANUFACTURER = "J.B. Limited";
+
+	void submerge
+
+	{
+		currentSubmergeDepth = 50;
+		// some code goes here.
+
+	}
+
+	void surface
+
+	{
+		// some code goes here
+
+	}
+
+}
