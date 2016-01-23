d63891169d4cefe89ecab7df6c383f7a816721aa
Jonathan Prince <prince.jon@gmail.com>
2016-01-23 11:48:54 -0400

    proper calctax code inserted.

--- Ch3/src/ch3/Tax.java
+++ Ch3/src/ch3/Tax.java
@@ -7,15 +7,15 @@ public class Tax {
 	
 	public double calcTax()
 	{
-	
-		double stateTax = 0;
-		if(grossIncome < 30000) {
-			stateTax=grossIncome*0.05;
+		double stateTax=0;
+		if(grossIncome <30000) {
+			stateTax = grossIncome * 0.05;
 		}
-		else{
+		else {
 			stateTax=grossIncome*0.06;
 		}
 		return stateTax;
 	}
+
 	
 }
