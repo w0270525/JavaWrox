7feb343f0b3f4332fc83bf63708dcb97b20028bd
Jonathan Prince <w0270525@nscc.ca>
2016-03-09 17:43:19 -0400

    formatting TestMain.java

--- Ch9/src/org/test/TestMain.java
+++ Ch9/src/org/test/TestMain.java
@@ -2,17 +2,15 @@ package org.test;
 
 import java.util.Scanner;
 
-public class TestMain
-{
-	public static final void main(String[] args)
-	{
-		Scanner scanner= new Scanner(System.in);
+public class TestMain {
+	public static final void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
 		String input = null;
 		System.out.println("Hello World");
-	
-	input = scanner.nextLine();	
+
+		input = scanner.nextLine();
 		System.out.println("You typed " + input);
 
-scanner.close();
+		scanner.close();
 	}
 }
